ad23be4d267b8dabf068ab56484546f9dcc0c6b6
sasriawesome <sasri.project@gmail.com>
2020-04-06 00:16:06 +0700

    Initial tests

--- django_numerators/__init__.py
+++ django_numerators/__init__.py
@@ -1,1 +1,7 @@
-default_app_config = 'django_numerators.apps.AppConfig'
+from django_numerators.utils.version import get_version
+
+# major.minor.patch.release.number
+# release must be one of alpha, beta, rc, or final
+VERSION = (0, 0, 1, 'alpha', 1)
+
+__version__ = get_version(VERSION)
---
+++ django_numerators/tests.py
@@ -0,0 +1,6 @@
+from django.test import TestCase
+
+
+class TestNumeratedModel(TestCase):
+    def test_run_test(self):
+        self.assertEqual(1, 2 - 1)
